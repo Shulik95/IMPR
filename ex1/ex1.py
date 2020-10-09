0bc180ba74939e5de884cbe0b2c428114697f742
Shalom Kachko <shalom.kachko@mail.huji.ac.il>
2020-10-09 21:11:45 +0300

    rgb2yiq done

--- ex1/ex1.py
+++ ex1/ex1.py
@@ -1,11 +1,21 @@
+# ---------- imports ---------- #
 import numpy as np
 from imageio import imread, imwrite
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
 
+# ---------- const definitions ---------- #
 MAX_GRAY_SCALE = 255
 TWO_DIM = 2
 THREE_DIM = 3
+
+# transformation matrix for rgb to yiq #
+rgb2yiq_matrix = np.array([[0.299, 0.587, 0.114],
+                           [0.596, -0.275, -0.321],
+                           [0.212, -0.523, 0.311]])
+
+
+# ---------- functions ---------- #
 
 
 def read_image(file_name, representation):
@@ -33,11 +43,31 @@ def imdisplay(filename, representation):
     :param filename: same as is read_image
     :param representation: same as in read_image
     """
-    plt.imshow(read_image(filename, representation))
+    plt.imshow(read_image(filename, representation), cmap='gray')
     plt.show()
 
+
+def rgb2yiq(imRGB):
+    """
+    transforms an RGB image into the YIQ color space.
+    :param imRGB: image to transform.
+    :return: the YIQ values as a matrix
+    """
+    ret_arr = np.zeros(shape=imRGB.shape, dtype=np.float64)  # same size matrix
+    for i in range(THREE_DIM):
+        ret_arr[:, :, i] = rgb2yiq_matrix[i][0] * imRGB[:, :, 0] +\
+                           rgb2yiq_matrix[i][1] * imRGB[:, :, 1] +\
+                           rgb2yiq_matrix[i][2] * imRGB[:, :, 2]
+    return ret_arr
+
+
+def yiq2rgb(imYIQ):
+    """
+    transforms an YIQ image into the RGB color space.
+    :param imYIQ: image to transform.
+    :return:
+    """
+    
 
 if __name__ == '__main__':
-    plt.imshow(read_image("graygoose.jpg", 1), cmap="gray")
-    plt.show()
-    pass
+    print(rgb2yiq(read_image("rgbimage.jpg", 2)).ndim)
